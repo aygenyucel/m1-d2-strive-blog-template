5d0ecc39f738ee751d91e050204276bddfd37e1d
Aygen Yucel <80033565+aygenyucel@users.noreply.github.com>
2022-12-21 18:55:19 +0300

    posting article from new article form

--- src/views/new/New.jsx
+++ src/views/new/New.jsx
@@ -5,28 +5,70 @@ import "react-quill/dist/quill.snow.css";
 import "./styles.css";
 const NewBlogPost = (props) => {
   const [text, setText] = useState("");
+  const [title, setTitle] = useState("");
+  const [category, setCategory] = useState("");
+  const [content, setContent] = useState("");
   const handleChange = useCallback((value) => {
     setText(value);
   });
+
+  const postArticle = async () => {
+    try {
+      let response = await fetch("http://localhost:3001/blogPosts", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          category: category,
+          title: title,
+          cover:
+            "https://imageio.forbes.com/specials-images/imageserve/5ef3f7eec4f2390006f0c356/GUI--Graphical-User-Interface--concept-/960x0.jpg?format=jpg&width=960",
+          readTime: {
+            value: 2,
+            unit: "minute",
+          },
+          author: {
+            name: "Aygen Yucel",
+            avatar: "https://ui-avatars.com/api/?name=Aygen+Yucel",
+          },
+          content: content,
+          createdAt: "NEW DATE",
+        }),
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const handleSumbit = () => {
+    postArticle();
+  };
   return (
     <Container className="new-blog-container">
-      <Form className="mt-5">
+      <Form className="mt-5" onSubmit={handleSumbit}>
         <Form.Group controlId="blog-form" className="mt-3">
-          <Form.Label>Title</Form.Label>
+          <Form.Label value={title} onChange={() => setTitle(title)}>
+            Title
+          </Form.Label>
           <Form.Control size="lg" placeholder="Title" />
         </Form.Group>
         <Form.Group controlId="blog-category" className="mt-3">
-          <Form.Label>Category</Form.Label>
+          <Form.Label value={category} onChange={() => setCategory(category)}>
+            Category
+          </Form.Label>
           <Form.Control size="lg" as="select">
-            <option>Category1</option>
-            <option>Category2</option>
-            <option>Category3</option>
+            <option>Tech</option>
+            <option>Business</option>
+            <option>Sports</option>
             <option>Category4</option>
             <option>Category5</option>
           </Form.Control>
         </Form.Group>
         <Form.Group controlId="blog-content" className="mt-3">
-          <Form.Label>Blog Content</Form.Label>
+          <Form.Label value={content} onChange={() => setContent(content)}>
+            Blog Content
+          </Form.Label>
           <ReactQuill
             value={text}
             onChange={handleChange}
